498dbeb61e42c7b1c4e78be9866119f012fd2cb7
Justin Edwards <justinledwards@gmail.com>
2014-04-09 15:15:19 -0400

    hello world

---
+++ Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Surplusing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.typeBox = new System.Windows.Forms.TextBox();
+            this.courtBox = new System.Windows.Forms.TextBox();
+            this.countyBox = new System.Windows.Forms.TextBox();
+            this.stateBox = new System.Windows.Forms.TextBox();
+            this.serialBox = new System.Windows.Forms.TextBox();
+            this.makeBox = new System.Windows.Forms.TextBox();
+            this.modelBox = new System.Windows.Forms.TextBox();
+            this.useablePartsBox = new System.Windows.Forms.TextBox();
+            this.reuseableEquipmentBox = new System.Windows.Forms.TextBox();
+            this.surplusReasonBox = new System.Windows.Forms.TextBox();
+            this.savePrintButton = new System.Windows.Forms.Button();
+            this.typeLabel = new System.Windows.Forms.Label();
+            this.courtLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // typeBox
+            // 
+            this.typeBox.Location = new System.Drawing.Point(122, 12);
+            this.typeBox.Name = "typeBox";
+            this.typeBox.Size = new System.Drawing.Size(185, 20);
+            this.typeBox.TabIndex = 0;
+            this.typeBox.TextChanged += new System.EventHandler(this.typeBox_TextChanged);
+            // 
+            // courtBox
+            // 
+            this.courtBox.Location = new System.Drawing.Point(122, 39);
+            this.courtBox.Name = "courtBox";
+            this.courtBox.Size = new System.Drawing.Size(184, 20);
+            this.courtBox.TabIndex = 1;
+            // 
+            // countyBox
+            // 
+            this.countyBox.Location = new System.Drawing.Point(122, 66);
+            this.countyBox.Name = "countyBox";
+            this.countyBox.Size = new System.Drawing.Size(184, 20);
+            this.countyBox.TabIndex = 2;
+            this.countyBox.TextChanged += new System.EventHandler(this.countyBox_TextChanged);
+            // 
+            // stateBox
+            // 
+            this.stateBox.Location = new System.Drawing.Point(122, 93);
+            this.stateBox.Name = "stateBox";
+            this.stateBox.Size = new System.Drawing.Size(184, 20);
+            this.stateBox.TabIndex = 3;
+            this.stateBox.TextChanged += new System.EventHandler(this.stateBox_TextChanged);
+            // 
+            // serialBox
+            // 
+            this.serialBox.Location = new System.Drawing.Point(123, 120);
+            this.serialBox.Name = "serialBox";
+            this.serialBox.Size = new System.Drawing.Size(184, 20);
+            this.serialBox.TabIndex = 4;
+            // 
+            // makeBox
+            // 
+            this.makeBox.Location = new System.Drawing.Point(123, 147);
+            this.makeBox.Name = "makeBox";
+            this.makeBox.Size = new System.Drawing.Size(184, 20);
+            this.makeBox.TabIndex = 5;
+            // 
+            // modelBox
+            // 
+            this.modelBox.Location = new System.Drawing.Point(123, 174);
+            this.modelBox.Name = "modelBox";
+            this.modelBox.Size = new System.Drawing.Size(184, 20);
+            this.modelBox.TabIndex = 6;
+            // 
+            // useablePartsBox
+            // 
+            this.useablePartsBox.Location = new System.Drawing.Point(123, 201);
+            this.useablePartsBox.Name = "useablePartsBox";
+            this.useablePartsBox.Size = new System.Drawing.Size(184, 20);
+            this.useablePartsBox.TabIndex = 7;
+            // 
+            // reuseableEquipmentBox
+            // 
+            this.reuseableEquipmentBox.Location = new System.Drawing.Point(122, 228);
+            this.reuseableEquipmentBox.Name = "reuseableEquipmentBox";
+            this.reuseableEquipmentBox.Size = new System.Drawing.Size(185, 20);
+            this.reuseableEquipmentBox.TabIndex = 8;
+            // 
+            // surplusReasonBox
+            // 
+            this.surplusReasonBox.Location = new System.Drawing.Point(122, 255);
+            this.surplusReasonBox.Name = "surplusReasonBox";
+            this.surplusReasonBox.Size = new System.Drawing.Size(185, 20);
+            this.surplusReasonBox.TabIndex = 9;
+            // 
+            // savePrintButton
+            // 
+            this.savePrintButton.Location = new System.Drawing.Point(8, 279);
+            this.savePrintButton.Name = "savePrintButton";
+            this.savePrintButton.Size = new System.Drawing.Size(299, 23);
+            this.savePrintButton.TabIndex = 10;
+            this.savePrintButton.Text = "Save and Print";
+            this.savePrintButton.UseVisualStyleBackColor = true;
+            this.savePrintButton.Click += new System.EventHandler(this.savePrintButton_Click);
+            // 
+            // typeLabel
+            // 
+            this.typeLabel.AutoSize = true;
+            this.typeLabel.Location = new System.Drawing.Point(85, 15);
+            this.typeLabel.Name = "typeLabel";
+            this.typeLabel.Size = new System.Drawing.Size(31, 13);
+            this.typeLabel.TabIndex = 11;
+            this.typeLabel.Text = "Type";
+            // 
+            // courtLabel
+            // 
+            this.courtLabel.AutoSize = true;
+            this.courtLabel.Location = new System.Drawing.Point(84, 42);
+            this.courtLabel.Name = "courtLabel";
+            this.courtLabel.Size = new System.Drawing.Size(32, 13);
+            this.courtLabel.TabIndex = 12;
+            this.courtLabel.Text = "Court";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "County";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "State";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(83, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Serial";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(82, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Make";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(80, 177);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Model";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(43, 204);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Useable Parts";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(5, 231);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(111, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Reuseable Equipment";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(16, 258);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(100, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Reason For Surplus";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 314);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.courtLabel);
+            this.Controls.Add(this.typeLabel);
+            this.Controls.Add(this.savePrintButton);
+            this.Controls.Add(this.surplusReasonBox);
+            this.Controls.Add(this.reuseableEquipmentBox);
+            this.Controls.Add(this.useablePartsBox);
+            this.Controls.Add(this.modelBox);
+            this.Controls.Add(this.makeBox);
+            this.Controls.Add(this.serialBox);
+            this.Controls.Add(this.stateBox);
+            this.Controls.Add(this.countyBox);
+            this.Controls.Add(this.courtBox);
+            this.Controls.Add(this.typeBox);
+            this.Name = "Form1";
+            this.Text = "Surplusing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox typeBox;
+        private System.Windows.Forms.TextBox courtBox;
+        private System.Windows.Forms.TextBox countyBox;
+        private System.Windows.Forms.TextBox stateBox;
+        private System.Windows.Forms.TextBox serialBox;
+        private System.Windows.Forms.TextBox makeBox;
+        private System.Windows.Forms.TextBox modelBox;
+        private System.Windows.Forms.TextBox useablePartsBox;
+        private System.Windows.Forms.TextBox reuseableEquipmentBox;
+        private System.Windows.Forms.TextBox surplusReasonBox;
+        private System.Windows.Forms.Button savePrintButton;
+        private System.Windows.Forms.Label typeLabel;
+        private System.Windows.Forms.Label courtLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Core;
+using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+
+
+
+namespace Surplusing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void countyBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void typeBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stateBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void savePrintButton_Click(object sender, EventArgs e)
+        {
+            
+            var surplusEntry = new Surplus {
+                  Type = typeBox.Text,
+                  Court = courtBox.Text,
+                  County = countyBox.Text,
+                  State = stateBox.Text,
+                  Serial = serialBox.Text,
+                  Make = makeBox.Text,
+                  Model = modelBox.Text,
+                  Useable_Parts = useablePartsBox.Text,
+                  Reuseable_Equipment = reuseableEquipmentBox.Text,
+                  Reason_for_Surplus = surplusReasonBox.Text,
+                  TimeStamp = DateTime.Now.ToString("MM-dd-yyyy hh:mm:ss")
+            };
+            SaveToExcel(surplusEntry);
+            PrintLabel(surplusEntry);
+            typeBox.Text = "";
+            courtBox.Text = "";
+            countyBox.Text = "";
+            stateBox.Text = "";
+            serialBox.Text = "";
+            makeBox.Text = "";
+            modelBox.Text = "";
+            useablePartsBox.Text = "";
+            reuseableEquipmentBox.Text = "";
+            surplusReasonBox.Text = "";
+            typeBox.Select();
+            typeBox.Focus();
+
+        }
+
+        public class Surplus
+        {
+            public string Type { get; set; }
+            public string Court { get; set; }
+            public string County { get; set; }
+            public string State { get; set; }
+            public string Serial { get; set; }
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public string Useable_Parts { get; set; }
+            public string Reuseable_Equipment { get; set; }
+            public string Reason_for_Surplus { get; set; }
+            public string TimeStamp { get; set; }
+
+        }
+
+
+        static void SaveToExcel(Surplus surplus)
+        {
+            // Set file location to desktop
+            string savefolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string xlfilename = savefolder + "\\surplus.xlsx";
+            var excelApp = new Excel.Application();
+            // Make excel invisible and turn off "file already exists alert before save".
+            excelApp.Visible = false;
+            excelApp.DisplayAlerts = false;
+
+
+            // Check if file exists, if not create it and then open it at the end.
+            if (File.Exists(xlfilename))
+            {
+            }
+            else
+            {
+                var newworkbook = excelApp.Workbooks.Add();
+                newworkbook.SaveAs(xlfilename, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            };
+            var workbook = excelApp.Workbooks.Open(xlfilename, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+
+            // Just set the column header each time, less code and work right now than checking it all and then setting it. 
+            Excel._Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
+
+            workSheet.Cells[1, "A"] = "Type";
+            workSheet.Cells[1, "B"] = "Court";
+            workSheet.Cells[1, "C"] = "County";
+            workSheet.Cells[1, "D"] = "State";
+            workSheet.Cells[1, "E"] = "Serial";
+            workSheet.Cells[1, "F"] = "Make";
+            workSheet.Cells[1, "G"] = "Model";
+            workSheet.Cells[1, "H"] = "Useable Parts";
+            workSheet.Cells[1, "I"] = "Reuseable Equipment";
+            workSheet.Cells[1, "J"] = "Reason for Surplus";
+            workSheet.Cells[1, "K"] = "TimeStamp";
+
+            var row = workSheet.UsedRange.Row +workSheet.UsedRange.Rows.Count - 1;
+
+            row++;
+            workSheet.Cells[row, "A"] = surplus.Type;
+            workSheet.Cells[row, "B"] = surplus.Court;
+            workSheet.Cells[row, "C"] = surplus.County;
+            workSheet.Cells[row, "D"] = surplus.State;
+            workSheet.Cells[row, "E"] = surplus.Serial;
+            workSheet.Cells[row, "F"] = surplus.Make;
+            workSheet.Cells[row, "G"] = surplus.Model;
+            workSheet.Cells[row, "H"] = surplus.Useable_Parts;
+            workSheet.Cells[row, "I"] = surplus.Reuseable_Equipment;
+            workSheet.Cells[row, "J"] = surplus.Reason_for_Surplus;
+            workSheet.Cells[row, "K"] = surplus.TimeStamp;
+            workSheet.Columns[1].AutoFit();
+            workSheet.Columns[2].AutoFit();
+            workSheet.Columns[3].AutoFit();
+            workSheet.Columns[4].AutoFit();
+            workSheet.Columns[5].AutoFit();
+            workSheet.Columns[6].AutoFit();
+            workSheet.Columns[7].AutoFit();
+            workSheet.Columns[8].AutoFit();
+            workSheet.Columns[9].AutoFit();
+            workSheet.Columns[10].AutoFit();
+            workSheet.Columns[11].AutoFit();
+            
+            workbook.SaveAs(xlfilename, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
+             false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+             Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            workbook.Close();
+
+        }
+
+
+        public void FindAndReplace(Word.Application doc, string findText, string replaceWithText)
+        {
+            
+            //options
+            object matchCase = false;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundsLike = false;
+            object matchAllWordForms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiacritics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+            //execute find and replace
+            doc.Selection.Find.Execute(findText, ref matchCase, ref matchWholeWord,
+                ref matchWildCards, ref matchSoundsLike, ref matchAllWordForms, ref forward, ref wrap, ref format, replaceWithText, ref replace,
+                ref matchKashida, ref matchDiacritics, ref matchAlefHamza, ref matchControl);
+        }
+        public void PrintLabel(Surplus surplus)
+        {
+            string savefolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string wordFilename = savefolder + "\\surplus.docx";
+            Word.Application wordApp = new Word.Application { Visible = false };
+            wordApp.DisplayAlerts = Word.WdAlertLevel.wdAlertsNone;
+            Word.Document wordDoc = wordApp.Documents.Open(wordFilename, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            wordDoc.Activate();
+
+            FindAndReplace(wordApp, "{type}", surplus.Type);
+            FindAndReplace(wordApp, "{court}", surplus.Court);
+            FindAndReplace(wordApp, "{county}", surplus.County);
+            FindAndReplace(wordApp, "{state}", surplus.State);
+            FindAndReplace(wordApp, "{serial}", surplus.Serial);
+            FindAndReplace(wordApp, "{make}", surplus.Make);
+            FindAndReplace(wordApp, "{model}", surplus.Model);
+            FindAndReplace(wordApp, "{useable_parts}", surplus.Useable_Parts);
+            FindAndReplace(wordApp, "{reuseable}", surplus.Reuseable_Equipment);
+            //FindAndReplace(wordApp, "{type}", surplus.Reason_for_Surplus);
+            FindAndReplace(wordApp, "{date}", DateTime.Now.ToString("MM-dd-yyyy"));
+            wordDoc.PrintOut();
+            wordDoc.Close(SaveChanges: false);
+            wordApp.Quit(Type.Missing, Type.Missing, Type.Missing);
+
+        }
+    }
+}
